76d530b117833b31ecd5bc7ee4a1bd797e26bd9b
Bianca Gualter <bncgualter@gmail.com>
2021-03-30 03:47:55 -0300

    🚧  saving user in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -10,4 +11,27 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+
+  jwt: {
+      signingKey: process.env.SIGNING_KEY,
+  },
+
+  callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                    )  
+                )
+
+                return true 
+            } catch {
+                return false
+            }
+        },
+    }
 })
