3cf0b851849d5f8b506dfc0a6e786fb528f4772b
Bianca Gualter <bncgualter@gmail.com>
2021-04-03 02:56:39 -0300

    🚧  listing screen posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,21 @@ import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { GetStaticProps } from 'next'
 import { getPrismicClient } from '../../services/prismic'
+import { RichText } from 'prismic-dom'
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+    slug: string
+    title: string
+    excerpt: string
+    updatedAt: string
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,34 +25,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="">
-                        <time>2 de abril de 2021</time>
-                        <strong>Creating a Monorepo with Lenna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Nihil iusto tempora veniam dolorum sapiente assumenda laborum.
-                        </p>
-                    </a>
-                    <a href="">
-                        <time>2 de abril de 2021</time>
-                        <strong>Creating a Monorepo with Lenna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Nihil iusto tempora veniam dolorum sapiente assumenda laborum.
-                        </p>
-                    </a>
-                    <a href="">
-                        <time>2 de abril de 2021</time>
-                        <strong>Creating a Monorepo with Lenna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Nihil iusto tempora veniam dolorum sapiente assumenda laborum.
-                        </p>
-                    </a>
-                    <a href="">
-                        <time>2 de abril de 2021</time>
-                        <strong>Creating a Monorepo with Lenna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Nihil iusto tempora veniam dolorum sapiente assumenda laborum.
-                        </p>
-                    </a>
+                    { posts.map(post => (
+                        <a key={post.slug} href="">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
@@ -57,11 +48,22 @@ export const getStaticProps: GetStaticProps = async () => {
         pageSize: 100,
     })
 
-    // console.log(JSON.stringify(response, null, 2))
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        }
+    })
 
     return {
         props: {
-
+            posts
         }
     }
 }
